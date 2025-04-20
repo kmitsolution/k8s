6f7e093eeef1d6f7879f3f4458cf229435bdb965
kmitsolution <84008107+kmitsolution@users.noreply.github.com>
2025-04-20 11:56:57 +0530

    Update bootstrap.sh

--- K8s-Vagrant/bootstrap.sh
+++ K8s-Vagrant/bootstrap.sh
@@ -1,66 +1,56 @@
 #!/bin/bash
 
-## !IMPORTANT ##
-#
-## This script is tested only in the generic/ubuntu2004 Vagrant box
-## If you use a different version of Ubuntu or a different Ubuntu Vagrant box test this again
-#
-
-echo "[TASK 1] Disable and turn off SWAP"
-sed -i '/swap/d' /etc/fstab
-swapoff -a
-
-echo "[TASK 2] Stop and Disable firewall"
-systemctl disable --now ufw >/dev/null 2>&1
-
-echo "[TASK 3] Enable and Load Kernel modules"
-cat >>/etc/modules-load.d/containerd.conf<<EOF
-overlay
-br_netfilter
-EOF
-modprobe overlay
-modprobe br_netfilter
-
-echo "[TASK 4] Add Kernel settings"
-cat >>/etc/sysctl.d/kubernetes.conf<<EOF
-net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables  = 1
-net.ipv4.ip_forward                 = 1
-EOF
-sysctl --system >/dev/null 2>&1
-
-echo "[TASK 5] Install containerd runtime"
-# apt update -qq >/dev/null 2>&1
-# apt install -qq -y containerd apt-transport-https >/dev/null 2>&1
-#mkdir /etc/containerd
-#containerd config default > /etc/containerd/config.toml
-#systemctl restart containerd
-#systemctl enable containerd >/dev/null 2>&1
-apt update
-apt install docker.io -y
-systemctl enable docker >/dev/null 2>&1
-apt-get install -y apt-transport-https curl
-
-
-echo "[TASK 6] Add apt repo for kubernetes"
-sudo mkdir /etc/apt/keyrings
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list >/dev/null 2>&1
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg >/dev/null 2>&1
-
-echo "[TASK 7] Install Kubernetes components (kubeadm, kubelet and kubectl)"
+##Update the system's package list and install necessary things:
+echo "Step 1- Setting up Kuberentes Environment by Raman"
 sudo apt-get update
-sudo apt-get install -y kubelet kubeadm kubectl >/dev/null 2>&1
+sudo apt install apt-transport-https curl -y
 
-echo "[TASK 8] Enable ssh password authentication"
+
+## Install containerd
+echo "Step 2- Installing containerd"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install containerd.io -y
+
+## Create containerd configuration
+
+sudo mkdir -p /etc/containerd
+sudo containerd config default | sudo tee /etc/containerd/config.toml
+
+sudo sed -i -e 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
+
+sudo systemctl restart containerd
+sudo systemctl enable containerd
+echo "Step 3 - Installing kubernetes components"
+## Install Kubernetes
+
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+sudo systemctl enable --now kubelet
+
+## Disable swap
+echo "Step 4 -Disabling swap"
+sudo swapoff -a
+sudo modprobe br_netfilter
+sudo sysctl -w net.ipv4.ip_forward=1
+
+echo "Step 5-Enable ssh password authentication"
 sed -i 's/^PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config
 echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
 systemctl reload sshd
 
-echo "[TASK 9] Set root password"
+echo "Step 6-  Set root password"
 echo -e "kubeadmin\nkubeadmin" | passwd root >/dev/null 2>&1
 echo "export TERM=xterm" >> /etc/bash.bashrc
 
-echo "[TASK 10] Update /etc/hosts file"
+echo "Step 7- Update /etc/hosts file"
 cat >>/etc/hosts<<EOF
 172.16.16.100   kmaster.example.com     kmaster
 172.16.16.101   kworker1.example.com    kworker1
