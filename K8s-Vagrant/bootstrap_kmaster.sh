f36812138656297f2413da437823425f84445be2
kmitsolution <84008107+kmitsolution@users.noreply.github.com>
2025-04-20 12:01:05 +0530

    Update bootstrap_kmaster.sh

--- K8s-Vagrant/bootstrap_kmaster.sh
+++ K8s-Vagrant/bootstrap_kmaster.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-apt update
-apt install docker.io -y
+
 
 echo "[TASK 1] Pull required containers"
 kubeadm config images pull >/dev/null 2>&1
 
 echo "[TASK 2] Initialize Kubernetes Cluster"
-kubeadm init --apiserver-advertise-address=172.16.16.100 --pod-network-cidr=192.168.0.0/16 >> /root/kubeinit.log 2>/dev/null
+kubeadm init --pod-network-cidr=10.244.0.0/16
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-echo "[TASK 3] Deploy Calico network"
-kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://docs.projectcalico.org/v3.18/manifests/calico.yaml >/dev/null 2>&1
+echo "[TASK 3] Deploy flannel network"
+kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml >/dev/null 2>&1
 
 echo "[TASK 4] Generate and save cluster join command to /joincluster.sh"
-kubeadm token create --print-join-command > /joincluster.sh 2>/dev/null
+kubeadm token create --print-join-command > tmp/joincluster.sh 2>/dev/null
