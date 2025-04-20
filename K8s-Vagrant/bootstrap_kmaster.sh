c7828de51e64a50bd808fdd2a4329d68429828c6
kmitsolution <84008107+kmitsolution@users.noreply.github.com>
2025-04-20 14:53:41 +0530

    Update bootstrap_kmaster.sh

--- K8s-Vagrant/bootstrap_kmaster.sh
+++ K8s-Vagrant/bootstrap_kmaster.sh
@@ -5,7 +5,7 @@ echo "[TASK 1] Pull required containers"
 kubeadm config images pull >/dev/null 2>&1
 
 echo "[TASK 2] Initialize Kubernetes Cluster"
-kubeadm init --pod-network-cidr=10.244.0.0/16
+kubeadm init --apiserver-advertise-address=172.16.16.100 --pod-network-cidr=10.244.0.0/16
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
